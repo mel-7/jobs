1477c0fdbd5b16ae3204c1424e02657978d042cf
Romel Indemne <romel.i@gagroup.net>
2020-03-03 16:57:53 +0400

    Initial commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,39 +15,28 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">
+        {{-- <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet"> --}}
 
     </head>
     <body>
         <div id="app">
-            <template>
-                <v-card
-                  color="grey lighten-4"
-                  flat
-                  height="200px"
-                  tile
-                >
-                  <v-toolbar dense>
-                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-              
-                    <v-toolbar-title>Title</v-toolbar-title>
-              
-                    <v-spacer></v-spacer>
-              
-                    <v-btn icon>
-                      <v-icon>mdi-magnify</v-icon>
-                    </v-btn>
-              
-                    <v-btn icon>
-                      <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-              
-                    <v-btn icon>
-                      <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                  </v-toolbar>
-                </v-card>
-              </template>
+            <v-card>
+                <div class="d-flex flex-row">
+                    <div class="logo">
+                        <a href="/" class="d-flex">
+                            <img src="https://gagroup.net/wp-content/uploads/2017/09/GHASSAN-ABOUD-GROUP-LOGO-03-e1562127584772.png" alt="Ghassan Aboud Group">
+                        </a>
+                    </div>  
+                    <v-text-field
+                    solo
+                    flat
+                    hide-details
+                    label="Search"
+                    append-inner-icon="mdi-magnify"
+                    style="border:1px solid #eee;"
+                    ></v-text-field>
+                </div>
+            </v-card>
         </div>
         <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
