118ff50ee5ff2637ec8abf9e23070b8a5aeaf6be
mel <indemnedev@gmail.com>
2020-06-17 21:33:06 +0400

    Employer -- Working on job form

--- resources/js/components/employer/jobs/JobForm.vue
+++ resources/js/components/employer/jobs/JobForm.vue
@@ -17,33 +17,108 @@
         </v-stepper-header>
         <v-stepper-items>
           <v-stepper-content step="1">
-            <h4 class="mb-3">Job Details</h4>
+            <h3 class="mb-5">Job Details</h3>
+            <label for>Job Title</label>
             <v-text-field
               single-line
               outlined
               dense
-              v-model="job.position"
-              label="position"
+              v-model="job.title"
+              label="Ex. Head of Human Resources"
               required
-              :rules="positionRules"
-              :error="positionError"
-              :error-messages="positionErrMsg"
+              :rules="textRules"
+              :error="textError"
+              :error-messages="textErrMsg"
               @change="clearAlert"
             ></v-text-field>
-            <v-text-field
+            <label for>Job Role</label>
+            <v-autocomplete
+              v-model="job.role"
+              :items="roles"
               single-line
               outlined
-              dense
-              v-model="job.slug"
-              label="slug"
               required
-              @mousedown="generateSlug"
-              @focus="generateSlug"
-              :rules="slugRules"
-              :error="slugError"
-              :error-messages="slugErrMsg"
+              dense
+              label="Select Job Role"
+            ></v-autocomplete>
+            <label for>Company Industry</label>
+            <v-autocomplete
+              v-model="job.industry"
+              :items="industries"
+              single-line
+              outlined
+              required
+              dense
+              chips
+              small-chips
+              label="Select your Company Industry"
+              multiple
+            >
+              <template v-slot:selection="data">
+                <v-chip
+                  v-bind="data.attrs"
+                  :input-value="data.selected"
+                  close
+                  @click="data.select"
+                  @click:close="remove(data.item)"
+                >{{ data.item }}</v-chip>
+              </template>
+              <template v-slot:item="data">
+                <v-list-item-content v-text="data.item"></v-list-item-content>
+              </template>
+            </v-autocomplete>
+            <label for>Job Location</label>
+            <v-autocomplete
+              v-model="job.location"
+              :items="roles"
+              single-line
+              outlined
+              required
+              dense
+              label="Select your Job Location"
+            ></v-autocomplete>
+            <label for>Employment Type</label>
+            <v-autocomplete
+              v-model="job.location"
+              :items="roles"
+              single-line
+              outlined
+              required
+              dense
+              label="Select Employment Type"
+            ></v-autocomplete>
+            <label for>Remote Job?</label>
+             <v-switch v-model="job.remote" :label="`${job.remote.toString()}`"></v-switch>
+            <label for>Monthly Salary</label>
+            <v-autocomplete
+              v-model="job.location"
+              :items="roles"
+              single-line
+              outlined
+              required
+              dense
+              label="Monthly Salary"
+            ></v-autocomplete>
+            <label for>Number of Vacancy</label>
+            <v-autocomplete
+              v-model="job.location"
+              :items="roles"
+              single-line
+              outlined
+              required
+              dense
+              label="Select"
+            ></v-autocomplete>
+            <label for>Job Description</label>
+            <v-textarea
+              dense
+              v-model="job.description"
+              label="content"
+              outlined
+              name="input-7-4"
               @change="clearAlert"
-            ></v-text-field>
+            ></v-textarea>
+            <label for>Desired Skills</label>
             <v-textarea
               dense
               v-model="job.content"
@@ -75,18 +150,17 @@
             <v-btn text>Cancel</v-btn>
           </v-stepper-content>
           <v-stepper-content step="4">
-           <div class="d-flex pa-4">
-                <v-spacer></v-spacer>
-                <v-btn
-                  text
-                  class="primary--text"
-                  :disabled="!valid"
-                  @click="save('draft')"
-                >Save as Draft</v-btn>
-                <v-btn :disabled="!valid" @click="save('publish')" color="primary">Publish</v-btn>
+            <div class="d-flex pa-4">
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                class="primary--text"
+                :disabled="!valid"
+                @click="save('draft')"
+              >Save as Draft</v-btn>
+              <v-btn :disabled="!valid" @click="save('publish')" color="primary">Publish</v-btn>
             </div>
           </v-stepper-content>
-
         </v-stepper-items>
       </v-stepper>
     </v-form>
@@ -160,37 +234,37 @@ export default {
         Add questions to learn about candidates
       */
       e1: 1,
-
       job: {
         position: "",
         slug: "",
         content: "",
-        status: ""
+        status: "",
+        role: "",
+        industry: [],
+        remote: 0
       },
-
+      roles: ["foo", "bar", "fizz", "buzz"],
+      industries: ["foo", "bar", "fizz", "buzz"],
       // ui
       valid: true,
 
       // Rules
-      positionRules: [
-        value => !!value || "Required",
-        value => (value && value.length < 50) || "Max 50 characters",
-        value => (value && value.length > 1) || "Min 1 characters"
-      ],
-      slugRules: [
+      textRules: [
         value => !!value || "Required",
         value => (value && value.length < 50) || "Max 50 characters",
         value => (value && value.length > 1) || "Min 1 characters"
       ],
 
       // Error Handling
-      positionError: false,
-      positionErrMsg: "",
-      slugError: false,
-      slugErrMsg: ""
+      textError: false,
+      textErrMsg: ""
     };
   },
   methods: {
+    remove(item) {
+      const index = this.job.industry.indexOf(item);
+      if (index >= 0) this.job.industry.splice(index, 1);
+    },
     generateSlug() {
       this.job.slug = this.job.position && slugify(this.job.position);
     },
@@ -203,7 +277,7 @@ export default {
 </script>
 
 <style lang="css">
-.v-stepper__step__step .v-icon.v-icon{
-    font-size: 14px !important;
+.v-stepper__step__step .v-icon.v-icon {
+  font-size: 14px !important;
 }
 </style>
